8a9ec3304a5b5020a58ab6e3eaa437c564266f78
Diógenes Dauster <diogenes.dauster@gmail.com>
2018-11-06 10:34:18 -0300

    feat: testing hooks with counter example

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React,{ useState } from "react";
+
+function Counter(){
+  const [count,setCount] = useState(0)
+
+  return (
+    <div>
+      <p>You click {count} times</p>
+      <button onClick={()=> setCount(count+1)}>Click Me</button>
+    </div>
+  )
+}
+
+export default Counter
