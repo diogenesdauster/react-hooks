8d316e699fa95f77c0e687d39cc62e519f3e06b0
Diógenes Dauster <diogenes.dauster@gmail.com>
2018-11-07 19:17:03 -0300

    refactor: create a Costum Hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,29 @@
 import React,{ useState, useEffect } from "react";
 
 function Counter(){
-  const [count,setCount] = useState(0)
+  const aCounter = useCount(0)
+
+  return (
+    <div>
+      <p>You click {aCounter[0]} times</p>
+      <button onClick={aCounter[1]}>Click Me</button>
+    </div>
+  )
+}
+
+function useCount(props){
+  const [count,setCount] = useState(props)
 
   useEffect(()=>{
     document.title = `You clicked ${count} times`;
   })
 
-  return (
-    <div>
-      <p>You click {count} times</p>
-      <button onClick={()=> setCount(count+1)}>Click Me</button>
-    </div>
-  )
+  function countOne(){
+    setCount(count+1)
+  }
+
+  return [count,countOne]
+
 }
 
 export default Counter
